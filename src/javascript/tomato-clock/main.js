962ed67fd43f7188a8a52bb17d7f014d0f25e81d
Matt Thomson <red.cataclysm@gmail.com>
2015-12-26 01:10:34 -0500

    Implements Timer Start/Pause

--- src/javascript/tomato-clock/main.js
+++ src/javascript/tomato-clock/main.js
@@ -4,10 +4,12 @@
   var sessionDuration = 25 * SECONDS;
 
   var currentTime;
+  var intervalId;
 
   function updateDisplay() {
     updateBreak();
     updateSession();
+    updateTimer();
   }
 
   function updateBreak() {
@@ -20,6 +22,16 @@
     $('#session .controls .duration').html(sessionDuration / SECONDS);
   }
 
+  function updateTimer() {
+    console.log('Updating Timer');
+    var text = '';
+    text += parseInt(currentTime / SECONDS)
+    text += ':';
+    text += currentTime % SECONDS < 10 ? '0' : '';
+    text += currentTime % SECONDS;
+    $('#timer .timer').html(text);
+  }
+
   function decrimentBreak() {
     if (breakDuration >= SECONDS) {
       breakDuration -= SECONDS;
@@ -30,6 +42,7 @@
   function decrimentSession() {
     if (sessionDuration >= SECONDS) {
       sessionDuration -= SECONDS;
+      currentTime = sessionDuration;
       updateSession();
     }
   }
@@ -41,14 +54,34 @@
 
   function incrimentSession() {
     sessionDuration += SECONDS;
+    currentTime = sessionDuration;
     updateSession();
   }
 
+  function startTimer() {
+    intervalId = setInterval(timer, 1000);
+    $('#timer').off();
+    $('#timer').on('click', pauseTimer);
+  }
+
+  function timer() {
+    currentTime--;
+    updateTimer();
+  }
+
+  function pauseTimer() {
+    clearInterval(intervalId);
+    $('#timer').off();
+    $('#timer').on('click', startTimer);
+  }
+
   $(document).ready(function() {
+    currentTime = sessionDuration;
     updateDisplay();
     $('#break .controls .less').on('click', decrimentBreak);
     $('#break .controls .more').on('click', incrimentBreak);
     $('#session .controls .less').on('click', decrimentSession);
     $('#session .controls .more').on('click', incrimentSession);
+    $('#timer').on('click', startTimer);
   });
 })();
